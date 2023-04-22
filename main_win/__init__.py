b5da10282dbd55eef10c5fab5155844dc05b4d8a
HYDxGenius <hyd20010211@gmail.com>
2023-04-22 16:19:10 +0100

    add files

---
+++ main_win/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+"""
+@Author: barrylee9527
+@Filename: __init__.py
+@Createdtime: 2023/4/16 13:28
+@Description: 
+"""
+
+if __name__ == '__main__':
+    pass
